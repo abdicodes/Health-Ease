1261dac3c489aff7cdd977e083f642a13e29bf7d
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-07-17 01:29:45 +0300

    feat: types and interfaces for events

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,78 +1,94 @@
 export enum EventTypes {
-  DoctorVisit = 'doctorVisit',
-  NurseVisit = 'nurseVisit',
-  Admission = 'admission',
+  DoctorVisit = 'Doctor Visit',
+  NurseVisit = 'Nurse Visit',
+  Admission = 'Admission',
   Discharge = 'discharge',
-  InPatientVisit = 'inPatientVisit',
-  Lab = 'labTest',
+  InPatientVisit = 'In-patient Visit',
+  Lab = 'Lab Test',
   Scan = 'scan',
-  Prescription = 'prescription',
+  Prescription = 'Prescription',
 }
 
 interface Drug {
   name: string
   quantity: number
+  dose: string
 }
 
 export interface BasicEvent {
   id: number
   type: EventTypes
-  dateTime: Date
+  dateTime: string
   comments?: string
 }
 
 export interface DoctorVisit extends BasicEvent {
+  type: EventTypes.DoctorVisit
   diagnosis: string
   doctorName: string
   details: string
 }
 
 export interface NurseVisit extends BasicEvent {
+  type: EventTypes.NurseVisit
   diagnosis: string
   nurseName: string
   details: string
 }
 
 export interface Admission extends BasicEvent {
+  type: EventTypes.Admission
   diagnosis: string
   doctorName: string
   details: string
 }
 
 export interface Discharge extends BasicEvent {
+  type: EventTypes.Discharge
   doctorName: string
   details: string
 }
 
 export interface InPatientVisit extends BasicEvent {
+  type: EventTypes.InPatientVisit
   diagnosis: string
   doctorName: string
   details: string
 }
 
-export interface LabTest extends BasicEvent {
+export interface Lab extends BasicEvent {
+  type: EventTypes.Lab
   tests: string[]
   doctorName: string
-  comments?: string
   technicianName?: string
   results: string[]
   pending: boolean
 }
 
 export interface Scan extends BasicEvent {
+  type: EventTypes.Scan
   scan: string
   doctorName: string
-  comments?: string
   technicianName?: string
   results: string
   pending: boolean
 }
 
 export interface Prescription extends BasicEvent {
+  type: EventTypes.Prescription
   drugs: Drug[]
   doctorName: string
-  comments?: string
   pharmacist?: string
   active: boolean
-  expirayDate: Date
+  expirayDate: string
 }
+
+export type Event =
+  | DoctorVisit
+  | NurseVisit
+  | Admission
+  | InPatientVisit
+  | Discharge
+  | Lab
+  | Scan
+  | Prescription
