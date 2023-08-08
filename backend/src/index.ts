fa2323320f4d6a516aeb300ade22db5ca71cec54
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-08-09 02:28:40 +0300

    refactor: moved files + feat: basic express server

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import { connectToDatabase } from './utils/db'
+import config from './utils/config'
+
+const app = express()
+app.use(express.json())
+
+// const allowedOrigins = ['http://localhost:3000']
+
+// const options: cors.CorsOptions = {
+//   origin: allowedOrigins,
+// }
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors())
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here')
+  res.send('pong')
+})
+
+const start = async (): Promise<void> => {
+  await connectToDatabase()
+  app.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
+    // logger.info(`Server running on port ${PORT}`)
+  })
+}
+
+void start()
+
+module.exports = app
