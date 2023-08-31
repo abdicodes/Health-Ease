6559c17e34e7b04da080873773fd3a6f65ec6a23
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-08-31 18:01:37 +0300

    refactor: added prescription model&migration file

--- backend/src/migrations/5_tests_prescription_table_init.ts
+++ backend/src/migrations/5_tests_prescription_table_init.ts
@@ -82,11 +82,52 @@ module.exports = {
         defaultValue: DataTypes.NOW,
       },
     })
+
+    await queryInterface.createTable('prescription_events', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      patient_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'patients', key: 'id' },
+      },
+      ordered_by: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'staffs', key: 'id' },
+      },
+
+      processed_by: {
+        type: DataTypes.INTEGER,
+        references: { model: 'staffs', key: 'id' },
+      },
+      drugs: {
+        type: DataTypes.ARRAY(DataTypes.JSONB), // Store an array of drugs objects here
+      },
+      comments: {
+        type: DataTypes.STRING,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    })
   },
 
-  // rollback action, here we drop lab_events and scan_events tables
+  // rollback action, here we drop lab_events, scan_events and prescription_events tables
   down: async ({ context: queryInterface }: { context: QueryInterface }) => {
     await queryInterface.dropTable('scan_events')
     await queryInterface.dropTable('lab_events')
+    await queryInterface.dropTable('prescription_events')
   },
 }
--- backend/src/models/tests_prescription.ts
+++ backend/src/models/tests_prescription.ts
@@ -78,4 +78,42 @@ ScanEvent.init(
   }
 )
 
+class PrescriptionEvent extends Model {}
+
+PrescriptionEvent.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    patientId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'patients', key: 'id' },
+    },
+    orderedBy: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'staffs', key: 'id' },
+    },
+    processedBy: {
+      type: DataTypes.INTEGER,
+      references: { model: 'staffs', key: 'id' },
+    },
+    drugs: {
+      type: DataTypes.ARRAY(DataTypes.JSONB), // Store an array of drugs objects here
+    },
+    comments: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: 'labEvent',
+  }
+)
+
 export { LabEvent, ScanEvent }
