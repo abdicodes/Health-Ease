248a6aa2fc52ade60bbf45d1300566f96aa69b61
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-09-21 17:10:25 +0300

    refactor: moved Selected form to own file

---
+++ frontend/src/components/staffPortal/doctorPage/entryForm/SelectedForm.tsx
@@ -0,0 +1,46 @@
+import { EntryFormValues } from '../../../../types'
+import AdmissionForm from './AdmissionForm'
+import AppointmentForm from './AppointmentForm'
+import DischargeForm from './DischargeForm'
+import EmergencyForm from './EmergencyForm'
+import InpatientForm from './InpatientForm'
+import LabForm from './Labform'
+import OutpatientForm from './OutPatientForm'
+import ScanForm from './ScanForm'
+
+const SelectedForm = ({
+  handleSubmit,
+  patientId,
+  selectedForm,
+}: {
+  handleSubmit: (values: EntryFormValues) => Promise<void>
+  patientId: number
+  selectedForm: string | null
+}) => {
+  switch (selectedForm) {
+    case 'outpatient':
+      return <OutpatientForm patientId={patientId} onSubmit={handleSubmit} />
+    case 'inpatient':
+      return <InpatientForm patientId={patientId} onSubmit={handleSubmit} />
+    case 'admission':
+      return <AdmissionForm patientId={patientId} onSubmit={handleSubmit} />
+    case 'emergency':
+      return <EmergencyForm patientId={patientId} onSubmit={handleSubmit} />
+    case 'discharge':
+      return <DischargeForm patientId={patientId} onSubmit={handleSubmit} />
+    case 'lab':
+      return <LabForm patientId={patientId} onSubmit={handleSubmit} />
+    case 'scan':
+      return <ScanForm patientId={patientId} onSubmit={handleSubmit} />
+    case 'appointment':
+      return <AppointmentForm patientId={patientId} onSubmit={handleSubmit} />
+    default:
+      return (
+        <div className="flex justify-center mt-1 text-neutral-700">
+          Select the entry type
+        </div>
+      )
+  }
+}
+
+export default SelectedForm
