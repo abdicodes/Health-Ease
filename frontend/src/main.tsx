d9a459093030ad1c5a2491982bdbd70a43fd6a3a
Abdirahman Suldaan <suldaan.89@hotmail.com>
2024-10-08 01:58:38 +0300

    bug fix + expose backend port

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,7 @@ import App from './App'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import React from 'react'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AuthProvider>
     <Router>
